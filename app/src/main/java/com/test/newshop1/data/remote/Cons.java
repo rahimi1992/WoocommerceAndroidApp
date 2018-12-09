b14f2f473a122f41fe176e9e57c7a2d77989a7fa
Farid Rahimi <f.rahimi1992@gmail.com>
2018-12-10 01:39:24 +0330

    base url added

--- app/src/main/java/com/test/newshop1/data/remote/Cons.java
+++ app/src/main/java/com/test/newshop1/data/remote/Cons.java
@@ -1,8 +1,9 @@
 package com.test.newshop1.data.remote;
 
-class Cons {
+public class Cons {
     static final String COSTUMER_KEY = "";
     static final String COSTUMER_SECRET = "";
     static final String OAUTH_TOKEN ="";
     static final String OAUTH_SECRET= "";
+    public static final String BASE_URL = "https://your-domain.com/wp-json/wc/v2/";
 }
