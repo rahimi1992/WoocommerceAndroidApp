d2f018b6dd16cebf76ada08e3931df084c7cf982
farid <f.rahimi1992@gmail.com>
2018-12-09 18:40:42 +0330

    empty constant class added

---
+++ app/src/main/java/com/test/newshop1/data/remote/Cons.java
@@ -0,0 +1,8 @@
+package com.test.newshop1.data.remote;
+
+class Cons {
+    static final String COSTUMER_KEY = "";
+    static final String COSTUMER_SECRET = "";
+    static final String OAUTH_TOKEN ="";
+    static final String OAUTH_SECRET= "";
+}
