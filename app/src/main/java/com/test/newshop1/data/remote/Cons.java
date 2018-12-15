49ee12edef7a735c29d58368b838871b59d6f986
Farid Rahimi <f.rahimi1992@gmail.com>
2018-12-15 03:33:01 +0330

    delete cached Cons.java file

--- app/src/main/java/com/test/newshop1/data/remote/Cons.java
+++
@@ -1,9 +0,0 @@
-package com.test.newshop1.data.remote;
-
-public class Cons {
-    static final String COSTUMER_KEY = "";
-    static final String COSTUMER_SECRET = "";
-    static final String OAUTH_TOKEN ="";
-    static final String OAUTH_SECRET= "";
-    public static final String BASE_URL = "https://your-domain.com/wp-json/wc/v2/";
-}
