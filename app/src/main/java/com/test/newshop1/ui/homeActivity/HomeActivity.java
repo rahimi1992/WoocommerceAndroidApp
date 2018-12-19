7ad2c5475e125f524e50529e5d72f19160190283
farid <f.rahimi1992@gmail.com>
2018-12-19 20:06:58 +0330

    test refactor

--- app/src/main/java/com/test/newshop1/ui/homeActivity/HomeActivity.java
+++ app/src/main/java/com/test/newshop1/ui/homeActivity/HomeActivity.java
@@ -32,7 +32,6 @@ public class HomeActivity extends BaseActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
 
         SearchManager searchManager =
